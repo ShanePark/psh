304a9a9fd45540f0530e14de018e87114e12f7ec
shane <psh40963@naver.com>
2022-03-27 14:00:13 +0900

    StringToDeepArray bug fixed (cover \n)

--- src/test/java/com/tistory/shanepark/SToolTest.java
+++ src/test/java/com/tistory/shanepark/SToolTest.java
@@ -14,5 +14,10 @@ class SToolTest {
     void convertToIntArray() {
         Assertions.assertThat(STool.convertToIntArray("[[1,2],[3],[3],[]])")).isDeepEqualTo(new int[][]{{1, 2}, {3}, {3}, {}});
         Assertions.assertThat(STool.convertToIntArray("[[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]")).isDeepEqualTo(new int[][]{{20, 15, 1}, {20, 17, 0}, {50, 20, 1}, {50, 80, 0}, {80, 19, 1}});
+        Assertions.assertThat(STool.convertToIntArray("[[1,1,0,0,0],\n" +
+                " [1,1,1,1,0],\n" +
+                " [1,0,0,0,0],\n" +
+                " [1,1,0,0,0],\n" +
+                " [1,1,1,1,1]]")).isDeepEqualTo(new int[][]{{1, 1, 0, 0, 0}, {1, 1, 1, 1, 0}, {1, 0, 0, 0, 0}, {1, 1, 0, 0, 0}, {1, 1, 1, 1, 1}});
     }
 }
